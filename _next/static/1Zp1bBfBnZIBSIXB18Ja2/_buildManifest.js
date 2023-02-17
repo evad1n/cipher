26f2929b52aa65acb94aab9044c1a3ac82e87677
evad1n <50718218+evad1n@users.noreply.github.com>
2023-02-17 21:21:08 +0000

    Deploying to gh-pages from @ evad1n/cipher@1620a0e6a70a49ff1d6418c0b2463d79b1b765b4 🚀

---
+++ _next/static/1Zp1bBfBnZIBSIXB18Ja2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1d979e0c2fc738ce.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/demo":["static/chunks/pages/demo-0118f977882370f8.js"],sortedPages:["/","/_app","/_error","/demo"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
