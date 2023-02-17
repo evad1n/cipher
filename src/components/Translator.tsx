1620a0e6a70a49ff1d6418c0b2463d79b1b765b4
Will Dickinson <will@zonos.com>
2023-02-17 14:20:17 -0700

    debounce all options

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -1,9 +1,13 @@
 import { AnimationType } from '@/scripts/AnimationGroup';
+import { DEFAULT_CIPHER_OPTIONS } from '@/scripts/Cipher';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import {
   CipherStream,
   CipherStreamOptionsUpdate,
+  DEFAULT_CIPHER_STREAM_OPTIONS,
 } from '../scripts/CipherStream';
+
+import { useDebounce } from 'use-debounce';
 
 const ID = 'cipher-parent';
 
@@ -11,10 +15,21 @@ export const Translator = () => {
   const [input, setInput] = useState('');
 
   // Group options
-  const [canvasSize, setCanvasSize] = useState(400);
+  const [canvasSize, setCanvasSize] = useState(
+    DEFAULT_CIPHER_STREAM_OPTIONS.canvasSize
+  );
   // Individual options
-  const [animationsEnabled, setAnimationsEnabled] = useState(true);
-  const [animationType, setAnimationType] = useState<AnimationType>('lerp');
+  const [animationsEnabled, setAnimationsEnabled] = useState(
+    DEFAULT_CIPHER_OPTIONS.animationsEnabled
+  );
+  const [animationType, setAnimationType] = useState<AnimationType>(
+    DEFAULT_CIPHER_OPTIONS.animationType
+  );
+  const [startRadius, setStartRadius] = useState(DEFAULT_CIPHER_OPTIONS.radius);
+  const [ringGap, setRingGap] = useState(DEFAULT_CIPHER_OPTIONS.gap);
+  const [animationDuration, setAnimationDuration] = useState(
+    DEFAULT_CIPHER_OPTIONS.animationDuration
+  );
 
   const currentOptions: CipherStreamOptionsUpdate = useMemo(
     () => ({
@@ -22,10 +37,22 @@ export const Translator = () => {
       individualOptions: {
         animationsEnabled,
         animationType,
+        gap: ringGap,
+        radius: startRadius,
+        animationDuration,
       },
     }),
-    [animationsEnabled, animationType, canvasSize]
+    [
+      canvasSize,
+      animationsEnabled,
+      animationType,
+      ringGap,
+      startRadius,
+      animationDuration,
+    ]
   );
+
+  const [debouncedCurrentOptions] = useDebounce(currentOptions, 200);
 
   const cipherStream = useRef(
     new CipherStream({
@@ -41,8 +68,8 @@ export const Translator = () => {
   }, [words]);
 
   useEffect(() => {
-    cipherStream.current.updateOptions(currentOptions);
-  }, [currentOptions]);
+    cipherStream.current.updateOptions(debouncedCurrentOptions);
+  }, [debouncedCurrentOptions]);
 
   return (
     <div>
@@ -59,11 +86,11 @@ export const Translator = () => {
           <span>Canvas size</span>
           <input
             type="range"
-            name="animation-group-progress"
-            id="animation-group-progress"
+            name="canvas-size"
+            id="canvas-size"
             value={canvasSize}
             onChange={e => setCanvasSize(parseInt(e.target.value, 10))}
-            min={200}
+            min={50}
             max={1000}
           />
         </label>
@@ -100,6 +127,39 @@ export const Translator = () => {
             Ease-out-in
           </label>
         </div>
+
+        <label>
+          <span>Animation duration</span>
+          <input
+            type="range"
+            value={animationDuration}
+            onChange={e => setAnimationDuration(parseInt(e.target.value, 10))}
+            min={10}
+            max={2000}
+          />
+        </label>
+
+        <label>
+          <span>Start radius</span>
+          <input
+            type="range"
+            value={startRadius}
+            onChange={e => setStartRadius(parseInt(e.target.value, 10))}
+            min={1}
+            max={100}
+          />
+        </label>
+
+        <label>
+          <span>Ring gap</span>
+          <input
+            type="range"
+            value={ringGap}
+            onChange={e => setRingGap(parseInt(e.target.value, 10))}
+            min={1}
+            max={100}
+          />
+        </label>
       </div>
       <div id={ID} className="cipher-stream"></div>
     </div>
