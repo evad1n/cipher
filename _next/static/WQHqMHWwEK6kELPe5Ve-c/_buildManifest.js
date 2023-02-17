674f90390566a83d51b72955e2e8bb6e2b585ca5
evad1n <50718218+evad1n@users.noreply.github.com>
2023-02-17 19:58:22 +0000

    Deploying to gh-pages from @ evad1n/cipher@6e5fa26acc70ce1d9227f67502f07656b8c270a5 🚀

--- _next/static/WQHqMHWwEK6kELPe5Ve-c/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d07296bf98d5d17a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/demo":["static/chunks/pages/demo-92ca8450d59cb658.js"],sortedPages:["/","/_app","/_error","/demo"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ZusKyJBbQFFpyiBvoQvfh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cb3042e52942ed6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/demo":["static/chunks/pages/demo-0118f977882370f8.js"],sortedPages:["/","/_app","/_error","/demo"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
